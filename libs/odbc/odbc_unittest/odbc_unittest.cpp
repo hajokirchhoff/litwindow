ba3a223ced6025174bacde2befe765f300f1e2d0
Hajo Kirchhoff <hajo.kirchhoff@izt-labs.de>
2018-10-30 14:43:28 +0100

    odbc Unittest begonnen

--- libs/odbc/odbc_unittest/odbc_unittest.cpp
+++ libs/odbc/odbc_unittest/odbc_unittest.cpp
@@ -2,9 +2,41 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/auto_unit_test.hpp>
 
+
+#include "litwindow/dataadapter.h"
+#include "litwindow/odbc/table.h"
+#include "litwindow/odbc/connection.h"
+
 #define new DEBUG_NEW
+
+using namespace litwindow;
+
+const wstring sqLite3Connection = L"Driver={SQLite3 ODBC Driver};DATABASE=X:\\V4t.dat;stepapi=0;notxn=0;shortnames=0;longnames=0;nocreat=0;nowchar=0;fksupport=0;oemcp=0;bigint=0;jdconv=0";
+
+void odbc_test_1()
+{
+	odbc::connection c;
+
+	odbc::sqlreturn rc=c.open(sqLite3Connection);
+	BOOST_CHECK(rc.success());
+
+	odbc::statement stmt;
+	stmt.set_statement(L"DROP TABLE test");
+	rc = stmt.execute();
+
+	stmt.set_statement(LR"(
+CREATE TABLE test (
+	id int primary key,
+	val real,
+	textval text
+)
+)");
+	rc = stmt.execute();
+
+	BOOST_CHECK(rc.success());
+}
 
 BOOST_AUTO_TEST_CASE(simple_odbc_test)
 {
-
+	odbc_test_1();
 }
--- libs/odbc/odbc_unittest/stdafx.h
+++ libs/odbc/odbc_unittest/stdafx.h
@@ -9,9 +9,15 @@
 #pragma warning(disable: 4786 4180)  // debug symbol length truncated
 #pragma once
 #include "targetver.h"
-// tell MS VC8 to stop issuing tons of (well meaning, but stupid) warnings
+ 
+ // tell MS VC8 to stop issuing tons of (well meaning, but stupid) warnings
 #define _CRT_SECURE_NO_DEPRECATE
 #define _CRT_NONSTDC_NO_DEPRECATE
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define NOMINMAX
+ // Windows Header Files:
+#include <windows.h>
 
 #ifdef _DEBUG
 #define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
