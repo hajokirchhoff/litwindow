8a65615583d81abada43b9f29269c9e767c7d845
Oliver Rommelfanger <oliver.rommelfanger@izt-labs.de>
2026-02-10 15:57:19 +0100

    only define _CRT_SECURE_NO_WARNINGS and _SCL_SECURE_NO_WARNINGS if not already defined

--- libs/lwbase/unittest/stdafx.h
+++ libs/lwbase/unittest/stdafx.h
@@ -4,8 +4,14 @@
 //
 
 #pragma once
+
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+#endif
+#ifndef _SCL_SECURE_NO_WARNINGS
 #define _SCL_SECURE_NO_WARNINGS
+#endif
+
 #include "targetver.h"
 
 #include <stdio.h>
